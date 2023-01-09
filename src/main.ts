b286042e113b060e65e433837eabbf41aad72644
Ali mohsenPour <86446834+alimpp@users.noreply.github.com>
2023-01-09 14:13:14 -0800

    Base level one

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import i18n from './i18n'
-
-createApp(App).use(i18n).use(router).mount('#app')
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@/assets/scss/main.scss'
+createApp(App).use(i18n).use(router).use(pinia).mount('#app')
---
+++ src/stores/applicationTheme.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+export const applicationTheme = defineStore('theme' , {
+    state: () => ({themeStatus : 'dark'}) ,  
+    actions : {
+       changeThemeStatus(){
+        if(this.themeStatus === "light"){
+            this.themeStatus = "dark"
+        } else if(this.themeStatus === "dark"){
+            this.themeStatus = "light"
+        }
+       }
+    }
+})
