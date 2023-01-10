3997662f37631082638387f67fa051181a75afad
Ali mohsenPour <86446834+alimpp@users.noreply.github.com>
2023-01-10 15:50:29 -0800

    Add new changes

--- src/layout/dashboard/topbar.vue
+++ src/layout/dashboard/topbar.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="topbar-layout">
-    <profileModal />
-    <accountCard class="mx-3" />
-    <languageSwitcher />
-    <changeTheme class="mt-2 mx-2" />
+      <profileModal />
+      <accountCard class="mx-3" />
+      <languageSwitcher />
+      <changeTheme class="mt-2 mx-1" />
+      <i class="bi bi-wifi size_2xl mx-1"></i>
+      <div class="d-flex mx-5">
+        <i class="bi bi-instagram size_2xl mx-1"></i>
+        <i class="bi bi-telegram size_2xl mx-1"></i>
+        <i class="bi bi-facebook size_2xl mx-1"></i>
+        <i class="bi bi-whatsapp size_2xl mx-1"></i>
+        <i class="bi bi-twitter size_2xl mx-1"></i>
+        <i class="bi bi-twitch size_2xl mx-1"></i>
+        <i class="bi bi-youtube size_2xl mx-1"></i>
+      </div>
+    
   </div>
 </template>
 
