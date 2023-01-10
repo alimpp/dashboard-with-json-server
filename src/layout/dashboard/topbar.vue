6c89bb814c8ccdfecfadbb03f28ee853bcbe9d24
Ali mohsenPour <86446834+alimpp@users.noreply.github.com>
2023-01-10 05:01:09 -0800

    Add base components

--- src/layout/dashboard/topbar.vue
+++ src/layout/dashboard/topbar.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="topbar-layout">
-    <changeTheme />
+    <accountCard />
+       <languageSwitcher class="mx-3" />
+    <changeTheme class="mt-1" />
   </div>
 </template>
 
 <script setup>
 import changeTheme from "@/components/changeTheme";
+import languageSwitcher from "@/components/languageSwitcher";
+import accountCard from "@/components/cards/accountCard";
 </script>
 
 <style lang="scss" scoped>
 .topbar-layout {
   width: 100%;
   height: 70px;
+  display: flex;
+  padding: 10px 10px;
 }
 </style>
