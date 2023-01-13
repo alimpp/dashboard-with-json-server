b78d0c93997b61d0b423c54ea5955331583242d2
Ali mohsenPour <86446834+alimpp@users.noreply.github.com>
2023-01-12 17:32:09 -0800

    Add spinner for delete

--- src/components/cards/ticketsCard.vue
+++ src/components/cards/ticketsCard.vue
@@ -9,7 +9,16 @@
           <span class="size_very_small gray_color">{{ body }}</span>
           <div class="d-flex mt-2">
             <ticketsDeatailModal :email="email" :body="body" />
-            <i class="bi bi-trash3 px-2 pt-1 app_pointer" @click="deleteTicket(id)"></i>
+            <div
+              class="spinner-border spinner-border-sm mx-2 mt-2"
+              role="status"
+              v-if="loading"
+            ></div>
+            <i
+              v-else
+              class="bi bi-trash3 px-2 pt-1 app_pointer"
+              @click="deleteTicket(id)"
+            ></i>
           </div>
         </div>
       </template>
@@ -18,15 +27,22 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import baseCard from "@/components/base/baseCard";
 import ticketsDeatailModal from "@/components/modals/ticketsDeatailModal";
 import { ticketsApi } from "@/stores/api/ticketsApi";
 
-const ticketsApiModule = ticketsApi()
+const loading = ref(false);
+
+const ticketsApiModule = ticketsApi();
 
 const deleteTicket = (id) => {
-  return ticketsApiModule.deleteTickets(id)
-}
+  loading.value = true;
+  return ticketsApiModule.deleteTickets(id);
+  setTimeout(() => {
+    loading.value = false;
+  }, 3000);
+};
 
 const props = defineProps({
   email: {
